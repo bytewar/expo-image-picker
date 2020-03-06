e5259555e24f1d2e0882b7024d1c904e6c9c6f38
Abduselam <aabdu2833@gmail.com>
2020-03-06 20:43:54 +0300

    Image picker

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import logo from './assets/logo.png'
+import logo from './assets/logo.png';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+    if (permissionResult.granted === false) {
+      alert('permission bla bla bla');
+      return;
+    }
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    console.log(pickerResult);
+  }
   return (
     <View style={styles.container}>
-      <Image source={{uri: "https://unsplash.it/500/500"}} style={styles.logo} />
-      <Text style={styles.instructions}>To share a photo from your phone with a friend, just press the button below!</Text>
+      <Image source={logo} style={styles.logo} />
+      <Text style={styles.instructions}>To sare a photo from your phone with a friend, just press the button below!</Text>
       <TouchableOpacity
-        onPress={() => alert('Hello world!')}
+        onPress={openImagePickerAsync}
         style={styles.button}>
         <Text style={styles.buttonText}>Pick a photo</Text>
       </TouchableOpacity>
